cd6640af079e6c3606bf31f494c68d506d182464
Andres Lara <angillama@gmail.com>
2023-03-14 11:55:44 -0500

    Commit 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//alert("Hello from JS")
+
+
+function printProducts(productos){
+    productos.forEach((producto)=>{
+        console.log(producto)
+    })
+}
+
+printProducts(productos)
---
+++ recursos/productos.js
@@ -0,0 +1,14 @@
+let productos=[
+    {
+        "Nombre": "HTML css y Jquery",
+        "Autor": "Jose Antonio Recio",
+        "cantidad": "10",
+        "imagen": "images/imagen1.jpg"
+    },
+    {
+        "Nombre": "Domine Javascript",
+        "Autor": "Pedro perez",
+        "cantidad": "0",
+        "imagen": "images/imagen2.jpg"
+    }
+]
